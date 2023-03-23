cd00c2ad61aacf7c99ed8b1da1b5fa19603b8c39
tinatd <konstantina.stasinopoulou@teradata.com>
2023-03-23 16:22:08 +0100

    second commit

--- ruinstall.sh
+++ ruinstall.sh
@@ -13,17 +13,17 @@ export R_VERSION=4.2.2
 wget https://cdn.rstudio.com/r/ubuntu-2204/pkgs/r-${R_VERSION}_1_amd64.deb
 sudo gdebi r-${R_VERSION}_1_amd64.deb
 
-# #Install Rstudio server 
-# wget https://download2.rstudio.org/server/jammy/amd64/rstudio-server-2023.03.0-386-amd64.deb
-# sudo gdebi rstudio-server-2023.03.0-386-amd64.deb
+#Install Rstudio server
+wget https://download2.rstudio.org/server/jammy/amd64/rstudio-server-2023.03.0-386-amd64.deb
+sudo gdebi rstudio-server-2023.03.0-386-amd64.deb
 
-#Download and install Workbench:
-sudo apt-get install gdebi-core
-curl -O https://download2.rstudio.org/server/jammy/amd64/rstudio-workbench-2023.03.0-386.pro1-amd64.deb
-sudo gdebi rstudio-workbench-2023.03.0-386.pro1-amd64.deb
+# Download and install Workbench:
+# sudo apt-get install gdebi-core
+# curl -O https://download2.rstudio.org/server/jammy/amd64/rstudio-workbench-2023.03.0-386.pro1-amd64.deb
+# sudo gdebi rstudio-workbench-2023.03.0-386.pro1-amd64.deb
 
 
-#On installation:The Workbench configuration file (/etc/rstudio/rserver.conf) contains default configuration 
+#On installation:The Workbench configuration file (/etc/rstudio/rserver.conf) contains default configuration
 #to run Workbench using local launcher sessions, without SSL enabled, listening on port 8787.
 
 #Verify R installation
